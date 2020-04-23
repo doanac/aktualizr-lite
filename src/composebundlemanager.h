45e1a443851421c0f6ff0db26e8064b41d5fe5af
Andy Doan <andy@foundries.io>
2020-04-23 16:31:38 -0500

    compose-bundles: Add basic config and test logic
    Signed-off-by: Andy Doan <andy@foundries.io>

--- src/composebundlemanager.h
+++ src/composebundlemanager.h
@@ -5,13 +5,26 @@
 
 #define PACKAGE_MANAGER_COMPOSEBUNDLE "ostree+compose_bundles"
 
+class ComposeBundleConfig {
+ public:
+  ComposeBundleConfig(const PackageConfig &pconfig);
+
+  std::vector<std::string> bundles;
+  boost::filesystem::path bundles_root;
+  boost::filesystem::path compose_bin{"/usr/bin/docker-compose"};
+  bool docker_prune{true};
+};
+
 class ComposeBundleManager : public OstreeManager {
  public:
   ComposeBundleManager(const PackageConfig &pconfig, const BootloaderConfig &bconfig,
                        const std::shared_ptr<INvStorage> &storage, const std::shared_ptr<HttpInterface> &http)
-      : OstreeManager(pconfig, bconfig, storage, http) {}
+      : OstreeManager(pconfig, bconfig, storage, http), cfg_(pconfig) {}
 
   std::string name() const override { return PACKAGE_MANAGER_COMPOSEBUNDLE; };
+
+ private:
+  ComposeBundleConfig cfg_;
 };
 
 #endif  // COMPOSE_BUNDLES_H_
